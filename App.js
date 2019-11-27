f86b4b65b2f1d4b3211b67b5734470d9d201fc40
László Czellahó <laszlo.czellaho@gmail.com>
2019-11-27 15:34:43 +0100

    Continue

--- App.js
+++ App.js
@@ -1,21 +1,221 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import styled from 'styled-components';
+import { ScrollView, SafeAreaView } from 'react-native';
+import Card from './components/Card';
+import { NotificationIcon } from  './components/Icons';
+import Logo from './components/Logo';
+import Course from './components/Course';
 
 export default function App() {
-  const [outputText, setOutputText] = useState('Open up App.js to start working on your app! Hello belllo');
   return (
-    <View style={styles.container}>
-      <Text>{ outputText }</Text>
-      <Button title="Change text" onPress={() => setOutputText('Text changed')}/>
-    </View>
+    <Container>
+      <SafeAreaView>
+        <ScrollView
+          style={{ height: '100%' }}
+        >
+          <TitleBar>
+            <Avatar source={require('./assets/avatar.jpg')}></Avatar>
+            <Title>Welcome back, </Title>
+            <Name>Meng</Name>
+            <NotificationIcon
+              style={{
+                position: "absolute",
+                right: 20,
+                top: 5,
+              }}
+            />
+          </TitleBar>
+          <ScrollView
+            style={{
+              flexDirection: 'row',
+              padding: 20,
+              paddingLeft: 12,
+              paddingTop: 30,
+            }}
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+          >
+          { logos.map((logo, index) => (
+            <Logo
+              key={index}
+              image={logo.image}
+              text={logo.text}
+            >
+            </Logo>
+          ))}
+          </ScrollView>
+          <Subtitle>Continue learning</Subtitle>
+          <ScrollView
+            horizontal={true}
+            style={{ paddingBottom: 30 }}
+            showsHorizontalScrollIndicator={false}
+          >
+            { cards.map((card, index) => (
+              <Card
+                key={index}
+                title={card.title}
+                image={card.image}
+                caption={card.caption}
+                logo={card.logo}
+                subtitle={card.subtitle}
+              />
+            ))}
+          </ScrollView>
+          <Subtitle>Popular Courses</Subtitle>
+          { courses.map((course, index) => (
+            <Course
+              key={index}
+              image={course.image}
+              title={course.title}
+              subtitle={course.subtitle}
+              logo={course.logo}
+              author={course.author}
+              avatar={course.avatar}
+              caption={course.caption}
+            />
+          ))}
+        </ScrollView>
+      </SafeAreaView>
+    </Container>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const Container = styled.View`
+  flex: 1;
+  background: #f0f3f5;
+`;
+
+const Title = styled.Text`
+  font-size: 16px;
+  color: #b8bece;
+  font-weight: 500;
+`;
+
+const Name = styled.Text`
+  font-size: 20px;
+  color: #3c4560;
+  font-weight: bold;
+`;
+
+const TitleBar = styled.View`
+  width: 100%;
+  margin-top: 50px;
+  padding-left: 80px;
+`;
+
+const Avatar = styled.Image`
+  width: 44px;
+  height: 44px;
+  background: black;
+  border-radius: 22px;
+  margin-left: 20px;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+const Subtitle = styled.Text`
+  font-size: 15px;
+  color: #b8bece;
+  font-weight: 600;
+  margin-left: 20px;
+  margin-top: 20px;
+  text-transform: uppercase;
+`;
+
+const logos = [
+  {
+    image: require('./assets/logo-framerx.png'),
+    text: "Framer X"
   },
-});
+  {
+    image: require('./assets/logo-figma.png'),
+    text: "Figma"
+  },
+  {
+    image: require('./assets/logo-studio.png'),
+    text: "Studio"
+  },
+  {
+    image: require('./assets/logo-react.png'),
+    text: "React"
+  },
+  {
+    image: require('./assets/logo-swift.png'),
+    text: "Swift"
+  },
+  {
+    image: require('./assets/logo-sketch.png'),
+    text: "Sketch"
+  },
+];
+
+const cards = [
+  {
+    title: "React Native for Designers",
+    image: require('./assets/background11.jpg'),
+    subtitle: "React Native",
+    caption: "1 of 12 sections",
+    logo: require('./assets/logo-react.png')
+  },
+  {
+    title: "Styled Components",
+    image: require('./assets/background12.jpg'),
+    subtitle: "React Native",
+    caption: "2 of 12 sections",
+    logo: require('./assets/logo-react.png')
+  },
+  {
+    title: "Props and Icons",
+    image: require('./assets/background13.jpg'),
+    subtitle: "React Native",
+    caption: "3 of 12 sections",
+    logo: require('./assets/logo-react.png')
+  },
+  {
+    title: "Static Data and Loop",
+    image: require('./assets/background14.jpg'),
+    subtitle: "React Native",
+    caption: "4 of 12 sections",
+    logo: require('./assets/logo-react.png')
+  },
+];
+
+const courses = [
+  {
+    title: "Prototype in Invision Studio",
+    subtitle: "10 Sections",
+    image: require('./assets/background13.jpg'),
+    logo: require('./assets/logo-studio.png'),
+    author: "Meng To",
+    avatar: require('./assets/avatar.jpg'),
+    caption: "Design and interactive prototype"
+  },
+  {
+    title: "Prototype in Invision Studio",
+    subtitle: "10 Sections",
+    image: require('./assets/background11.jpg'),
+    logo: require('./assets/logo-framerx.png'),
+    author: "Meng To",
+    avatar: require('./assets/avatar.jpg'),
+    caption: "Design and interactive prototype"
+  },
+  {
+    title: "Prototype in Invision Studio",
+    subtitle: "10 Sections",
+    image: require('./assets/background14.jpg'),
+    logo: require('./assets/logo-invision.png'),
+    author: "Meng To",
+    avatar: require('./assets/avatar.jpg'),
+    caption: "Design and interactive prototype"
+  },
+  {
+    title: "Prototype in Invision Studio",
+    subtitle: "10 Sections",
+    image: require('./assets/background6.jpg'),
+    logo: require('./assets/logo-figma.png'),
+    author: "Meng To",
+    avatar: require('./assets/avatar.jpg'),
+    caption: "Design and interactive prototype"
+  },
+];
