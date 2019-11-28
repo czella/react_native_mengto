abb6227484df16d4da1d8002e0731de2d2722888
Laszlo Czellaho <laszlo.czellaho@gmail.com>
2019-11-28 17:11:41 +0100

    Add redux and animations

--- App.js
+++ App.js
@@ -1,221 +1,29 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
-import { ScrollView, SafeAreaView } from 'react-native';
-import Card from './components/Card';
-import { NotificationIcon } from  './components/Icons';
-import Logo from './components/Logo';
-import Course from './components/Course';
+import React from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import HomeScreen from "./screens/HomeScreen";
 
-export default function App() {
-  return (
-    <Container>
-      <SafeAreaView>
-        <ScrollView
-          style={{ height: '100%' }}
-        >
-          <TitleBar>
-            <Avatar source={require('./assets/avatar.jpg')}></Avatar>
-            <Title>Welcome back, </Title>
-            <Name>Meng</Name>
-            <NotificationIcon
-              style={{
-                position: "absolute",
-                right: 20,
-                top: 5,
-              }}
-            />
-          </TitleBar>
-          <ScrollView
-            style={{
-              flexDirection: 'row',
-              padding: 20,
-              paddingLeft: 12,
-              paddingTop: 30,
-            }}
-            horizontal={true}
-            showsHorizontalScrollIndicator={false}
-          >
-          { logos.map((logo, index) => (
-            <Logo
-              key={index}
-              image={logo.image}
-              text={logo.text}
-            >
-            </Logo>
-          ))}
-          </ScrollView>
-          <Subtitle>Continue learning</Subtitle>
-          <ScrollView
-            horizontal={true}
-            style={{ paddingBottom: 30 }}
-            showsHorizontalScrollIndicator={false}
-          >
-            { cards.map((card, index) => (
-              <Card
-                key={index}
-                title={card.title}
-                image={card.image}
-                caption={card.caption}
-                logo={card.logo}
-                subtitle={card.subtitle}
-              />
-            ))}
-          </ScrollView>
-          <Subtitle>Popular Courses</Subtitle>
-          { courses.map((course, index) => (
-            <Course
-              key={index}
-              image={course.image}
-              title={course.title}
-              subtitle={course.subtitle}
-              logo={course.logo}
-              author={course.author}
-              avatar={course.avatar}
-              caption={course.caption}
-            />
-          ))}
-        </ScrollView>
-      </SafeAreaView>
-    </Container>
-  );
-}
+const initialState = {
+  action: "",
+};
 
-const Container = styled.View`
-  flex: 1;
-  background: #f0f3f5;
-`;
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "OPEN_MENU":
+      return {action: "openMenu"};
+    case "CLOSE_MENU":
+      return {action: "closeMenu"};
+    default:
+      return state;
+  }
+};
 
-const Title = styled.Text`
-  font-size: 16px;
-  color: #b8bece;
-  font-weight: 500;
-`;
+const store = createStore(reducer);
 
-const Name = styled.Text`
-  font-size: 20px;
-  color: #3c4560;
-  font-weight: bold;
-`;
+const App = () => (
+  <Provider store={store}>
+    <HomeScreen />
+  </Provider>
+);
 
-const TitleBar = styled.View`
-  width: 100%;
-  margin-top: 50px;
-  padding-left: 80px;
-`;
-
-const Avatar = styled.Image`
-  width: 44px;
-  height: 44px;
-  background: black;
-  border-radius: 22px;
-  margin-left: 20px;
-  position: absolute;
-  top: 0;
-  left: 0;
-`;
-
-const Subtitle = styled.Text`
-  font-size: 15px;
-  color: #b8bece;
-  font-weight: 600;
-  margin-left: 20px;
-  margin-top: 20px;
-  text-transform: uppercase;
-`;
-
-const logos = [
-  {
-    image: require('./assets/logo-framerx.png'),
-    text: "Framer X"
-  },
-  {
-    image: require('./assets/logo-figma.png'),
-    text: "Figma"
-  },
-  {
-    image: require('./assets/logo-studio.png'),
-    text: "Studio"
-  },
-  {
-    image: require('./assets/logo-react.png'),
-    text: "React"
-  },
-  {
-    image: require('./assets/logo-swift.png'),
-    text: "Swift"
-  },
-  {
-    image: require('./assets/logo-sketch.png'),
-    text: "Sketch"
-  },
-];
-
-const cards = [
-  {
-    title: "React Native for Designers",
-    image: require('./assets/background11.jpg'),
-    subtitle: "React Native",
-    caption: "1 of 12 sections",
-    logo: require('./assets/logo-react.png')
-  },
-  {
-    title: "Styled Components",
-    image: require('./assets/background12.jpg'),
-    subtitle: "React Native",
-    caption: "2 of 12 sections",
-    logo: require('./assets/logo-react.png')
-  },
-  {
-    title: "Props and Icons",
-    image: require('./assets/background13.jpg'),
-    subtitle: "React Native",
-    caption: "3 of 12 sections",
-    logo: require('./assets/logo-react.png')
-  },
-  {
-    title: "Static Data and Loop",
-    image: require('./assets/background14.jpg'),
-    subtitle: "React Native",
-    caption: "4 of 12 sections",
-    logo: require('./assets/logo-react.png')
-  },
-];
-
-const courses = [
-  {
-    title: "Prototype in Invision Studio",
-    subtitle: "10 Sections",
-    image: require('./assets/background13.jpg'),
-    logo: require('./assets/logo-studio.png'),
-    author: "Meng To",
-    avatar: require('./assets/avatar.jpg'),
-    caption: "Design and interactive prototype"
-  },
-  {
-    title: "Prototype in Invision Studio",
-    subtitle: "10 Sections",
-    image: require('./assets/background11.jpg'),
-    logo: require('./assets/logo-framerx.png'),
-    author: "Meng To",
-    avatar: require('./assets/avatar.jpg'),
-    caption: "Design and interactive prototype"
-  },
-  {
-    title: "Prototype in Invision Studio",
-    subtitle: "10 Sections",
-    image: require('./assets/background14.jpg'),
-    logo: require('./assets/logo-invision.png'),
-    author: "Meng To",
-    avatar: require('./assets/avatar.jpg'),
-    caption: "Design and interactive prototype"
-  },
-  {
-    title: "Prototype in Invision Studio",
-    subtitle: "10 Sections",
-    image: require('./assets/background6.jpg'),
-    logo: require('./assets/logo-figma.png'),
-    author: "Meng To",
-    avatar: require('./assets/avatar.jpg'),
-    caption: "Design and interactive prototype"
-  },
-];
+export default App;
--- components/Course.js
+++ components/Course.js
@@ -5,14 +5,14 @@ const Course = props => (
   <Container>
     <Cover>
       <Image source={props.image} />
-      <Logo source={props.logo} />
+      <Logo source={props.logo} resizeMode="contain"/>
       <Subtitle>{props.subtitle}</Subtitle>
       <Title>{props.title}</Title>
     </Cover>
     <Content>
       <Avatar source={props.avatar} />
       <Caption>{props.caption}</Caption>
-      <Name>{props.name}</Name>
+      <Author>Taught by{props.author}</Author>
     </Content>
   </Container>
 );
@@ -25,39 +25,74 @@ const Container = styled.View`
   background: white;
   margin: 10px 20px;
   border-radius: 14px;
-  box-shadow: 0 10px 20px rgba(0, 0,  );
+  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.15);
 `;
 
 const Cover = styled.View`
   height: 260px;
+  border-top-left-radius: 14px;
+  border-top-right-radius: 14px;
+  overflow: hidden;
 `;
 
 const Image = styled.Image`
   width: 100%;
   height: 100%;
+  position: absolute;
 `;
 
 const Logo = styled.Image`
   height: 48px;
   width: 48px;
+  position: absolute;
+  top: 90px;
+  left: 50%;
+  margin-left: -24px;
 `;
 
 const Title = styled.Text`
+  font-size: 24px;
+  color: white;
+  font-weight: 600;
+  margin-top: 4px;
+  margin-bottom: 20px;
+  margin-left: 20px;
+  width: 170px;
 `;
 
 const Subtitle = styled.Text`
+  font-size: 15px;
+  font-weight: 500;
+  color: rgba(255, 255, 255, 0.8);
+  text-transform: uppercase;
+  margin-left: 20px;
 `;
 
 const Content = styled.View`
+  padding-left: 62px;
+  justify-content: center;
+  height: 75px;
 `;
 
 const Avatar = styled.Image`
+  position: absolute;
   width: 32px;
   height: 32px;
+  top: 20px;
+  left: 20px;
+  border-radius: 16px;
 `;
 
 const Caption = styled.Text`
+  font-size: 14px;
+  color: #3c4560;
+  font-weight: 500;
+  
 `;
 
-const Name = styled.Text`
+const Author = styled.Text`
+  font-size: 13px;
+  color: #b8bece;
+  font-weight: 500;
+  margin-top: 4px;
 `;
